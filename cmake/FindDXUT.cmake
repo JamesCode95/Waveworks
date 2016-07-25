adf1833a3b655bba5e42e91f738e4a758532493f
Jason Maskell <Jason Maskell>
2016-07-25 15:11:04 +0700

    Modified finders to take Visual Studio platform into account when finding libs. Will need to be modified further for use with PackMan

--- cmake/FindDXUT.cmake
+++ cmake/FindDXUT.cmake
@@ -11,22 +11,32 @@ endif()
 find_path(DXUT_SDK_PATH Core/DXUT.h
 	HINTS ${GW_DEPS_ROOT}/DXUT
 	)
-    
+   
+# Need to be smart and get the proper VS library here
+
+if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 18.0.0.0 AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 19.0.0.0)
+	SET(VS_STR "Desktop_2013")
+elseif(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 19.0.0.0)
+	SET(VS_STR "Desktop_2015")
+else()
+	MESSAGE(FATAL_ERROR "Failed to find compatible DXUT - Only supporting VS2013 and VS2015")
+endif()
+   
 find_library(DXUT_LIBRARY_RELEASE 
     NAMES DXUT
-    PATHS ${DXUT_SDK_PATH}/Core/Bin/*/${DXUTARCH}/Release)
+    PATHS ${DXUT_SDK_PATH}/Core/Bin/${VS_STR}/${DXUTARCH}/Release)
 
 find_library(DXUT_LIBRARY_DEBUG
     NAMES DXUT
-    PATHS ${DXUT_SDK_PATH}/Core/Bin/*/${DXUTARCH}/Debug)
+    PATHS ${DXUT_SDK_PATH}/Core/Bin/${VS_STR}/${DXUTARCH}/Debug)
     
 find_library(DXUT_Opt_LIBRARY_RELEASE
     NAMES DXUTOpt
-    PATHS ${DXUT_SDK_PATH}/Optional/Bin/*/${DXUTARCH}/Release)
+    PATHS ${DXUT_SDK_PATH}/Optional/Bin/${VS_STR}/${DXUTARCH}/Release)
 
 find_library(DXUT_Opt_LIBRARY_DEBUG
     NAMES DXUTOpt
-    PATHS ${DXUT_SDK_PATH}/Optional/Bin/*/${DXUTARCH}/Debug)
+    PATHS ${DXUT_SDK_PATH}/Optional/Bin/${VS_STR}/${DXUTARCH}/Debug)
     
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(DXUT 
     DEFAULT_MSG 
